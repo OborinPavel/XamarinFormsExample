bd31db8eef48f87517d778f49ae86c6822961732
Pavel <oborin.pavel@gmail.com>
2014-12-03 21:24:51 +1100

    map tweak

--- Xamarin.Forms.Example.Android/Views/GoogleMapAndroid.xaml.cs
+++ Xamarin.Forms.Example.Android/Views/GoogleMapAndroid.xaml.cs
@@ -9,23 +9,34 @@ namespace Xamarin.Forms.Example.Android
 {	
 	public partial class GoogleMapAndroid : ContentPage, IMapView
 	{
+//		Location _location;
+//		string _label;
+//		string _address;
+
 		#region IMapView implementation
 
 		public void AddPin(Location location, string label, string address) {
+			map = new Map { IsShowingUser = true };
 			var position = new Position (location.Lat, location.Long);
-			var span = new MapSpan (position, 0.01, 0.01);
-			map.MoveToRegion (span);
 			map.Pins.Add (new Pin { 
 				Type = PinType.Place, 
 				Position = position, 
 				Label = label, 
 				Address = address });
+			var span = new MapSpan (position, 0.01, 0.01);
+			map.MoveToRegion (span);
 		}
 		#endregion
 
 		public GoogleMapAndroid ()
 		{
 			InitializeComponent ();
+		}
+
+		protected override void OnDisappearing ()
+		{
+			base.OnDisappearing ();
+			map.Pins.Clear ();
 		}
 	}
 }
